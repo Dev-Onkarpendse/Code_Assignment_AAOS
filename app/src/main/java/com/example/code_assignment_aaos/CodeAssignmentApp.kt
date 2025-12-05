81d2e9034c3025985456f52d3306f617ea95dc88
Dev-Onkarpendse <pendseomkar92@gmail.com>
2025-12-06 00:37:51 +0530

    initialize Android Kotlin project with Hilt setup

---
+++ app/src/main/java/com/example/code_assignment_aaos/CodeAssignmentApp.kt
@@ -0,0 +1,14 @@
+package com.example.code_assignment_aaos
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Onkar Pendse  pendseomkar92@gmail.com
+ *  on 06-12-2025.
+ *  For More Information Contact me!!!.
+ *          !!  Thank You  !!
+ */
+@HiltAndroidApp
+class CodeAssignmentApp: Application() {
+}
